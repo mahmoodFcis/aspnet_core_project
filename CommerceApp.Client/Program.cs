cf4d1b28c9170fbcf5fbf342925679140918d47a
mshaaban <mshaaban@DESKTOP-MLCAK3H>
2019-01-31 21:51:07 +0200

    Adding saving samples

---
+++ CommerceApp.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using CommerceApp.DAL;
+using System;
+
+namespace CommerceApp.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductRepository productRepository = new ProductRepository();
+            productRepository.Add(new Data.Product() { Code = "PC", Name = "Juice", Price = 40 });
+
+        }
+    }
+}
---
+++ CommerceApp.DAL/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using CommerceApp.Data;
+using System;
+using System.Collections;
+
+namespace CommerceApp.DAL
+{
+    public class ProductRepository
+    {
+        CommerceContext db;
+        public ProductRepository()
+        {
+            db = new CommerceContext();
+        }
+        public void Add(Product p)
+        {
+            db.Products.Add(p);
+            db.SaveChanges();
+        }
+
+        public void AddRange(ICollection products)
+        {
+            db.AddRange(products);
+            db.SaveChanges();
+
+            
+        }
+    }
+}
